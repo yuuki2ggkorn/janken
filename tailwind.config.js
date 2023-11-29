593df6a81d361c8f39f2f3b79a5660ec647d6d94
yuuki2ggkorn <150794391+yuuki2ggkorn@users.noreply.github.com>
2023-11-29 10:01:17 +0900

    janken

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./mujun.html",
+    "./js/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
